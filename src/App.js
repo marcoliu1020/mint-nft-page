b74a37bb5d0e4d641daade8457a9ba6317e663e2
marco <a0931900802@gmail.com>
2022-07-18 16:47:20 +0800

    connect metamask

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import React from "react";
 
 function App() {
+  const [currentAccount, setCurrentAccount] = React.useState(null)
+  React.useEffect(() => {
+    window.ethereum.request({ method: 'eth_requestAccounts' })
+    .then(accounts => setCurrentAccount(accounts[0]))
+  }, [])
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Contract {}</h1>
+      <h1>Account {currentAccount}</h1>
+      <h1>NFT amount {}</h1>
+      <button className="mint-button">Mint</button>
     </div>
   );
 }
